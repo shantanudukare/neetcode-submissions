3ae237226b954836b23f22e4b3f15482bb74101a
shantanudukare <dukareshantanu2104@gmail.com>
2026-08-20 12:50:58 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int x=0;
+        for(int i=0;i<nums.size();i++){
+          
+          if(x<i){
+            return false;
+          }
+          x=max(x,i+nums[i]);
+        }
+        return true;
+    }
+};
